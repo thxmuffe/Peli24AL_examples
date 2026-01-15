5bbbda83b4eb624d6c590a497daa032346fc0b93
Paavo Penttila <thxmuffe@hotmail.com>
2026-01-15 12:54:57 +0200

    Use Days enumarator and check if user input monday

--- Door/Program.cs
+++ Door/Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
+        
+        public enum Days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please input day: ");
+            string day = Console.ReadLine().Trim();
+
+            if (day.Equals(Days.Monday.ToString(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine("It's Monday!");
+            }
+            else
+            {
+                Console.WriteLine("It's not Monday.");
+            }
+
         }
     }
 }
